7ec18f611b790d57331e4f56d88478f1ed9185f2
Will Carter <will.carter@fergusllc.com>
2019-12-13 04:56:25 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+def say_hello(name = "Ruby Programmer")
+  if name == "Hello Ruby Programmer!"
+    puts name
+  else
+    puts "Hello #{name}!"
+  end
+end
+
+say_hello
