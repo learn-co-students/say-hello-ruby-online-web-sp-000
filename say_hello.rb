591ae39a80d7fee76185a468f8ae767432e5611e
Ambar Khare <ambar.khare24@gmail.com>
2021-06-11 18:48:54 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
 # Build your say_hello method here
+
+#say_hello method (greets the user) defined with a default arguement
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+#say_hello(Ambar) will print Hello Ambar! and say_hello will print Hello Ruby Programmer!
